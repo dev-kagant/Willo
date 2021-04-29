ab1da3bd2bae97686e4281de1bc91e15168695ca
Kerri <gant.kerri@gmail.com>
2021-04-28 22:16:01 -0400

    Functions Done

--- pressure_analytics_exercise-main/analyze.py
+++ pressure_analytics_exercise-main/analyze.py
@@ -2,35 +2,56 @@
 
 import json
 import sys
+import csv
 
 def count_contractions(pressure_data):
     """
     Count the number of contractions for a pressure curve
-
     :param pressure_data: a list of pressure points
     :return: The total number of contractions
     """
     # FIXME
-    return 0
+    pressure_monitor = 95
+    contraction_counter = 0
+    for row in pressure_data:
+        if row[1] == 'pressure': continue
+        if float(row[1]) > float(pressure_monitor):
+            if pressure_monitor == 85:
+                pressure_monitor = 95
+                contraction_counter += 1
+            continue
+        if float(row[1]) < pressure_monitor:
+            if pressure_monitor == 95:
+                pressure_monitor = 85
+            continue
+        else: continue
 
-def contractions_per_sec(pressure_data):
+    return contraction_counter
+
+
+def contractions_per_sec(pressure_data, totalContractions):
     """
     Calculate the mean contractions / secs for a pressure curve
-
     :param pressure_data: a list of pressure points
     :return: The mean frequency of contraction / secs
     """
     # FIXME
-    return 0
+    total_seconds = pressure_data[len(pressure_data)-1]
+    return round(totalContractions/int(int(total_seconds[0])/1000), 2)
+
+
 
 def main():
     pressure_file = sys.argv[1]
-    n = 0 # FIXME
-    f = 0 # FIXME
-    print("---")
-    print("For {}:".format(pressure_file))
-    print("* Number of contraction = {}".format(n))
-    print("* Contraction / s = {}".format(n))
+    with open(sys.argv[1]) as pressure_file:   #opening and reading csv files
+        csv_reader = csv.reader(pressure_file, delimiter=",")
+        data = [row for row in csv_reader]
+        n = count_contractions(data) # 0 FIXME
+        f = contractions_per_sec(data, n) # 0 FIXME
+        print("---")
+        print("For {}:".format(pressure_file.name))
+        print("* Number of contraction = {}".format(n))
+        print("* Contraction / s = {}".format(f))
 
     return 0
 
